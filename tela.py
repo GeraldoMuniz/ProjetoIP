0e4984c46b9a86d140829c98c4e3672c92c03341
GabrielJustinoG <gjgs@cin.ufpe.br>
2026-06-28 11:49:16 -0300

    Update tela.py

--- tela.py
+++ tela.py
@@ -1,8 +1,8 @@
 import pygame
 #esta função gerencia o tamanho da tela a ser mostrada.
 def criar_tela ():
-    largura = 1920
-    altura = 1080
+    largura = 1672
+    altura = 941
     janela = pygame.display.set_mode ((largura, altura))
 
     return janela
