6a6a13319002351969fca0b2f068c57cf91d188e
GeraldoMuniz <geraldoneto9507@gmail.com>
2026-06-28 13:15:09 -0300

    implementaçao da POO nos personagens (boneco) e ajuste nos limites do mapa

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import tela
-from personagens import boneco
+from personagens.boneco import Boneco
 from objetos import chave
 import mapa 
 
@@ -8,7 +8,8 @@ def main ():
     pygame.init ()
     #cria a tela
     tela_jogo = tela.criar_tela ()
-
+    #cria o personagem
+    boneco_jogo = Boneco ()
     chaves_coletadas = 0
 
     flag_rodar = True
@@ -18,11 +19,9 @@ def main ():
             #se apertar no botão de sair, sai.
             if (ocorrencia.type == pygame.QUIT):
                 flag_rodar = False
-        #vê o movimento do boneco
-        boneco.movimento ()
+        #movimento do boneco
+        boneco_jogo.movimento ()
 
-        #colisao do boneco
-        rect_boneco = pygame.Rect(boneco.x, boneco.y, boneco.tamanho, boneco.tamanho)
         #logica chave
         for c in chave.lista_chaves:
             if not c["pegou"]:
@@ -30,15 +29,15 @@ def main ():
                 rect_chave = pygame.Rect(c["x"], c["y"], chave.tamanho, chave.tamanho)
                         
                         #se houver colisao
-                if rect_boneco.colliderect(rect_chave):
+                if boneco_jogo.rect.colliderect(rect_chave):
                     c["pegou"] = True #some
                     chaves_coletadas += 1
                     print(f"Chaves coletadas: {chaves_coletadas}")
         #pinta a tela
         mapa.desenhar(tela_jogo)
-        #desenha os boneco
+        #desenha os objetos
         chave.desenhar_chaves (tela_jogo)
-        pygame.draw.rect (tela_jogo, (50, 205, 50), (boneco.x, boneco.y, 40, 40))
+        pygame.draw.rect (tela_jogo, (50, 205, 50), boneco_jogo.rect)
         #atualiza a tela
         pygame.display.flip ()
     #encerra o programa
--- personagens/boneco.py
+++ personagens/boneco.py
@@ -1,31 +1,48 @@
 import pygame
-#define onde spawnar o boneco
-x, y = 400, 300
-#quanto ele vai andar
-velocidade = 1
-#tamanho dele
-tamanho = 40
-#função de locomoção
-def movimento ():
-    global x, y
 
-    teclas = pygame.key.get_pressed ()
+class Personagem (pygame.sprite.Sprite):                            #herda classe existente para facilitar
+    def __init__(self, nome, vida, velocidade, x, y, tamanho):
+        super().__init__()                      #recursos sprite
+        self.nome = nome                        #construtor
+        self.vida = vida
+        self.velocidade = velocidade
+        self.rect = pygame.Rect (x, y, tamanho, tamanho) #saber tudo sobre o personagem
+        self.tamanho = tamanho
 
-    if (teclas [pygame.K_LEFT] or teclas [pygame.K_a]):
-        x -= velocidade
-    elif (teclas [pygame.K_RIGHT] or teclas [pygame.K_d]):
-        x += velocidade
-    elif (teclas [pygame.K_UP] or teclas [pygame.K_w]):
-        y -= velocidade
-    elif (teclas [pygame.K_DOWN] or teclas [pygame.K_s]):
-        y += velocidade
-#checa se não vai ultrapassar o limite do mapa e deixa ele totalmente visível
-    if (x < 0):
-        x = 0
-    elif (x > 1920 - tamanho):
-        x = 1920 - tamanho
+    def limite_mapa (self, largura_mapa = 1672, altura_mapa = 941):
+        if (self.rect.x < 0):
+            self.rect.x = 0                                    #nao deixa ultrapassar os limites da tela
+        elif (self.rect.x > largura_mapa - self.tamanho):
+            self.rect.x = largura_mapa - self.tamanho
 
-    elif (y < 0):
-        y = 0
-    elif (y > 1080 - tamanho):
-        y = 1080 - tamanho
+        if (self.rect.y < 0):
+            self.rect.y = 0
+        elif (self.rect.y > altura_mapa - self.tamanho):
+            self.rect.y = altura_mapa - self.tamanho
+
+
+    def dano (self, quantidade):
+        self.vida -= quantidade
+        if (self.vida <= 0):            #em desenvolvimento
+            print ("perdeu")
+
+
+class Boneco (Personagem):      #herda de personagem
+    def __init__(self, nome = "Usuário", vida = 100, velocidade = 1, x = 400, y = 300, tamanho = 40): #valores padroes
+        super().__init__(nome, vida, velocidade, x, y, tamanho)     #construtor já é chamado
+
+    def movimento (self):    #especificidade (selecionar o boneco atual)
+        teclas = pygame.key.get_pressed ()
+
+        if teclas[pygame.K_LEFT] or teclas[pygame.K_a]:
+            self.rect.x -= self.velocidade
+        elif teclas[pygame.K_RIGHT] or teclas[pygame.K_d]:
+            self.rect.x += self.velocidade
+        
+        if teclas[pygame.K_UP] or teclas[pygame.K_w]:
+            self.rect.y -= self.velocidade
+        elif teclas[pygame.K_DOWN] or teclas[pygame.K_s]:
+            self.rect.y += self.velocidade
+
+
+        self.limite_mapa ()    #verifica se ele mesmo nao ultrapassou o limite
