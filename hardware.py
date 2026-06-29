09d3fd2a8ec2334c588c9628c1ac1c4fa93013dd
GabrielJustinoG <gjgs@cin.ufpe.br>
2026-06-29 14:15:16 -0300

    Update hardware.py

--- hardware.py
+++ hardware.py
@@ -1,6 +1,6 @@
 import pygame
 
-hardware = pygame.image.load("imagem/mapa_lab_hardware.png")
+hardware = pygame.image.load("imagem/mapa_lab_de_hardware.png")
 
 def desenhar(tela):
     tela.blit(hardware, (0, 0))
