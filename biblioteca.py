fdeb7a3e083c2ecc518fc911b8de4a1b2508d8ab
GabrielJustinoG <gjgs@cin.ufpe.br>
2026-06-28 23:53:55 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,6 +1,6 @@
 import pygame
 
-biblioteca = pygame.image.load("imagem/biblioteca.png")
+biblioteca = pygame.image.load("imagem/mapa_biblioteca.png")
 
 def desenhar(tela):
     tela.blit(biblioteca, (0, 0))
