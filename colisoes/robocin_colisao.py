625aa8d35a3f241582b50930c87fc83785173253
GabrielJustinoG <gjgs@cin.ufpe.br>
2026-07-01 08:14:33 -0300

    Update robocin_colisao.py

--- colisoes/robocin_colisao.py
+++ colisoes/robocin_colisao.py
@@ -1,5 +1,77 @@
 import pygame
 
 paredes_robocin = [
-    
+
+    # PAREDES EXTERNAS
+
+    #superior
+    pygame.Rect(34, 28, 1604, 18),
+
+    #inferior
+    pygame.Rect(34, 896, 1604, 18),
+
+    #esquerda (acima da porta)
+    pygame.Rect(70, 28, 18, 360),
+
+    #esquerda (abaixo da porta)
+    pygame.Rect(70, 500, 18, 414),
+
+    #direita
+    pygame.Rect(1620, 28, 18, 886),
+
+    #racks superior esquerdo
+
+    pygame.Rect(118, 66, 120, 310),
+    pygame.Rect(300, 55, 200, 160),
+
+    #parte superior central
+
+    pygame.Rect(565, 45, 430, 170),
+
+    #capsula robo
+
+    pygame.Rect(1070, 45, 115, 130),
+
+    #estante
+    pygame.Rect(1210, 65, 75, 100),
+
+    #quadro
+    pygame.Rect(1410, 65, 200, 90),
+   
+    #computadores direita
+
+    pygame.Rect(1480, 240, 120, 260),
+
+    #campo de futebol
+
+    pygame.Rect(1080, 228, 335, 235),
+
+    #plataforma central
+
+    pygame.Rect(555, 298, 400, 320),
+
+    #estação esquerda
+
+    pygame.Rect(60, 575, 335, 160),
+    pygame.Rect(120, 735, 350, 140),
+
+    #máquina azul
+    pygame.Rect(45, 760, 95, 135),
+
+    #parte central inferior
+
+    pygame.Rect(595, 705, 270, 120),
+
+    #rack
+    pygame.Rect(960, 680, 60, 90),
+
+    #bancada direita
+
+    pygame.Rect(1125, 520, 300, 155),
+
+    #mesa inferior direita
+    pygame.Rect(1155, 770, 190, 110),
+
+    #rack inferior direito
+    pygame.Rect(1515, 580, 70, 325),
 ]
