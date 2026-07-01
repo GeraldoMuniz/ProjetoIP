3355520fd26a0fbba19cde7eeee5060fc1f01e3d
GabrielJustinoG <gjgs@cin.ufpe.br>
2026-07-01 10:19:15 -0300

    Update sala_de_musica_colisao.py

--- colisoes/sala_de_musica_colisao.py
+++ colisoes/sala_de_musica_colisao.py
@@ -1,5 +1,83 @@
 import pygame
 
 paredes_sala_de_musica = [
-    
+
+    #PAREDES EXTERNAS
+
+    #superior esquerda
+    pygame.Rect(32, 30, 720, 18),
+
+    #superior direita
+    pygame.Rect(920, 30, 720, 18),
+
+    #esquerda
+    pygame.Rect(32, 30, 18, 880),
+
+    #direita
+    pygame.Rect(1622, 30, 18, 880),
+
+    #inferior
+    pygame.Rect(32, 890, 1608, 18),
+
+    #caixas de som 
+
+    pygame.Rect(78, 140, 115, 135),
+    pygame.Rect(1490, 135, 120, 135),    
+
+    pygame.Rect(78, 680, 100, 135),
+    pygame.Rect(1500, 680, 110, 135),
+
+    #estantes superiores
+
+    pygame.Rect(180, 92, 130, 28),
+    pygame.Rect(1115, 92, 135, 28),
+
+    #guitarras
+
+    pygame.Rect(180, 85, 600, 120),
+
+    #quadro
+
+    pygame.Rect(920, 85, 600, 135),
+
+    #cabos
+
+    pygame.Rect(1145, 205, 350, 45),
+
+    #piano 
+
+    pygame.Rect(380, 360, 270, 170),
+
+    #piano elétrico
+
+    pygame.Rect(930, 355, 220, 115),
+
+    #banco
+    pygame.Rect(980, 475, 60, 60),
+
+    #sofá
+
+    pygame.Rect(1250, 380, 80, 165),
+
+    #mesa de centro
+    pygame.Rect(1370, 420, 75, 120),
+
+    #bateria
+
+    pygame.Rect(670, 660, 265, 165),
+
+    #mesa de som
+
+    pygame.Rect(1220, 700, 210, 120),
+
+    #estante esqeurda inferior
+
+    pygame.Rect(190, 715, 165, 85),
+
+    #amplificadores
+
+    pygame.Rect(150, 360, 80, 90),
+    pygame.Rect(1500, 360, 65, 90),
+
+    pygame.Rect(1120, 620, 60, 60),
 ]
