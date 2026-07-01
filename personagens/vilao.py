2b841693705f2efd5454dd09f842681e68bc03a8
GabrielJustinoG <gjgs@cin.ufpe.br>
2026-07-01 17:06:11 -0300

    Update vilao.py

--- personagens/vilao.py
+++ personagens/vilao.py
@@ -8,6 +8,18 @@ class Inimigo (Personagem):
         self.visao = visao
         self.posicao = pygame.math.Vector2 (x, y)     #declaração do campo de visao e posição do vilao
 
+        self.sprite_frente = pygame.image.load("imagem/vilao_frente.png").convert_alpha()
+        self.sprite_costas = pygame.image.load("imagem/vilao_costas.png").convert_alpha()
+        self.sprite_direita = pygame.image.load("imagem/vilao_direita.png").convert_alpha()
+        self.sprite_esquerda = pygame.image.load("imagem/vilao_esquerda.png").convert_alpha()
+
+        self.sprite_frente = pygame.transform.scale(self.sprite_frente, (40, 56))
+        self.sprite_costas = pygame.transform.scale(self.sprite_costas, (40, 56))
+        self.sprite_direita = pygame.transform.scale(self.sprite_direita, (40, 56))
+        self.sprite_esquerda = pygame.transform.scale(self.sprite_esquerda, (40, 56))
+
+        self.sprite = self.sprite_frente
+
     def perseguir (self, alvo):
         posicao_alvo = pygame.math.Vector2 (alvo.rect.x, alvo.rect.y)
 
@@ -16,6 +28,18 @@ class Inimigo (Personagem):
 
         if (0 < distancia <= self.visao):
             direcao = vetor_direcao.normalize ()        #normalize deixa o vetor igual a 1, mas mantem apontando para o boneco
+
+        if abs(direcao.x) > abs(direcao.y):
+            if direcao.x > 0:
+                self.sprite = self.sprite_direita
+            else:
+                self.sprite = self.sprite_esquerda
+        else:
+            if direcao.y > 0:
+                self.sprite = self.sprite_frente
+            else:
+                self.sprite = self.sprite_costas
+
             self.posicao += direcao * self.velocidade       #o vetor que vale 1 é multiplicado pela velocidade a cada frame
 
             self.rect.x = int (self.posicao.x)
