f822edb9580ff626cb9cef3c58f30b9e5193aa81
GabrielJustinoG <gjgs@cin.ufpe.br>
2026-07-01 16:08:33 -0300

    Update dica.py

--- objetos/dica.py
+++ objetos/dica.py
@@ -7,9 +7,12 @@ class Dica(Coletavel):
         self.cor = cor
         self.mapa = mapa  #em qual sala essa dica aparece
 
-    def desenhar(self, tela):
-        if not self.pegou:
-            pygame.draw.rect(tela, self.cor, self.rect)
+        self.imagem = pygame.image.load("imagem/pista_transparente.png").convert_alpha()
+        self.imagem = pygame.transform.scale(self.imagem, (32, 32))
+
+        def desenhar(self, tela):
+            if not self.pegou:
+                tela.blit(self.imagem, self.rect)
 
 #dicas do jogo
 #o nome ja é o texto da dica
