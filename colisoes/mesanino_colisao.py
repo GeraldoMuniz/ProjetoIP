1c67e18121b8ac61355a658c3ab3bfb8ee9f02a2
GabrielJustinoG <gjgs@cin.ufpe.br>
2026-07-01 07:40:10 -0300

    Update mesanino_colisao.py

--- colisoes/mesanino_colisao.py
+++ colisoes/mesanino_colisao.py
@@ -1,5 +1,71 @@
 import pygame
 
 paredes_mesanino = [
-    
+
+    #PAREDES EXTERNAS
+
+    #superior
+    pygame.Rect(35, 38, 1602, 18),
+
+    #esquerda
+    pygame.Rect(35, 38, 18, 865),
+
+    #direita
+    pygame.Rect(1619, 38, 18, 865),
+
+    #inferior
+    pygame.Rect(35, 885, 1602, 18),
+
+    #PORTA
+
+    #esquerda da porta
+
+    pygame.Rect(720, 0, 70, 110),
+
+    #direita da porta
+    pygame.Rect(880, 0, 70, 110),
+
+    #bancada cozinha
+
+    pygame.Rect(225, 125, 510, 165),
+
+    #geladeiras juntas
+
+    pygame.Rect(995, 90, 215, 205),
+
+    #microondas
+
+    pygame.Rect(1215, 120, 125, 160),
+
+    #geladeira cinza
+
+    pygame.Rect(1325, 80, 135, 220),
+
+    #mesa esquerda superior
+
+    pygame.Rect(295, 382, 205, 125),
+
+    #mesa esquerda inferior
+
+    pygame.Rect(300, 640, 205, 125),
+
+    #mesa redonda direita
+
+    pygame.Rect(1100, 355, 180, 180),
+
+    #mesa direita inferior
+
+    pygame.Rect(1145, 640, 210, 125),
+
+    #mesa de madeira no centro
+
+    pygame.Rect(695, 610, 220, 225),
+
+    #armario esquerdo
+
+    pygame.Rect(85, 585, 85, 180),
+
+    #armario direito
+
+    pygame.Rect(1490, 450, 95, 295),
 ]
